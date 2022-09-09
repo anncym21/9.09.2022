46a448f515e4d6ee0143cf0083fd01faaf879daf
student-01 <cymanowska.anna@wp.pl>
2022-09-09 10:20:52 +0200

    przyciski

--- app/src/main/java/com/example/zad1/MainActivity.kt
+++ app/src/main/java/com/example/zad1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.zad1
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 //https://developer.android.com/develop/ui/views/layout/declaring-layout
 
@@ -9,5 +11,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val activity:MainActivity = MainActivity()
+
+        val button = findViewById<Button>(R.id.tak)
+        var clicks = 0
+        button.setOnClickListener {
+            clicks += 1
+
+            when(clicks){
+                button.setTypeface(Typeface.SANS_SERIF)
+                        button.text = "Sans Sherif"
+
+            }
+        }
+
+        close = findViewById<Button>(R.id.close)
+
+        close.SetOnClickListener{
+            activity.finish()
+
+            System.exit(0)
+        }
     }
 }
