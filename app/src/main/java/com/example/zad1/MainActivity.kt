8c38bdfccfe177968716e3849435860a520a6158
student-01 <cymanowska.anna@wp.pl>
2022-09-09 09:55:48 +0200

    przycisk, tlo i tekst

--- app/src/main/java/com/example/zad1/MainActivity.kt
+++ app/src/main/java/com/example/zad1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.zad1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//https://developer.android.com/develop/ui/views/layout/declaring-layout
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
